21521ce6f3c5c04db9eb95c8cc1e5d0a2d7fad5a
Igor3550 <igormeloigormelo@gmail.com>
2022-10-18 22:17:46 -0300

    configuring first components

---
+++ src/components/pages/signinPage/index.js
@@ -0,0 +1,13 @@
+import {
+  Container
+} from './style';
+
+const SignInPage = () => {
+  return (
+    <Container>
+      <h1>SigninPage</h1>
+    </Container>
+  )
+}
+
+export default SignInPage;
