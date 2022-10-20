de59b5339473dd9370e4e138a896251ffa0290cf
Igor3550 <igormeloigormelo@gmail.com>
2022-10-20 17:20:01 -0300

    feat: getting hashtags

--- src/components/TrendingComponent/index.js
+++ src/components/TrendingComponent/index.js
@@ -1,18 +1,44 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import {
   Container,
   TrendsArea
 } from './style'
 
-const trendingComponent = () => {
+function getHashtagList () {
+  const promise = axios.get('http://localhost:4000/hashtags')
+  return promise;
+}
+
+const TrendingComponent = () => {
+
+  const [hashtagList, setHashtagList] = useState([])
+
+  useEffect(() => {
+    const promise = getHashtagList()
+    promise.catch((error) => {
+      console.log(error)
+    })
+    promise.then((resp) => {
+      setHashtagList(resp.data)
+      console.lof(resp.data)
+    })
+  }, [])
+
   return (
     <Container>
       <h1>trending</h1>
       <TrendsArea>
-        <p>#javascript</p>
-        <p>#javascript</p>
+        {
+          hashtagList.map((hashtag) => {
+            return (
+              <p>{hashtag.text}</p>
+            )
+          })
+        }
       </TrendsArea>
     </Container>
   )
 }
 
-export default trendingComponent;
+export default TrendingComponent;
