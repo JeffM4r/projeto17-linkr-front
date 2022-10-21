e42a96a9d0ced3d7604cd4ddaa779609f6ae05ee
ana-aranha <anab.aranha@gmail.com>
2022-10-21 15:05:59 -0300

    feat: it's possible to publish new post

--- src/components/PublishComponent/index.js
+++ src/components/PublishComponent/index.js
@@ -1,81 +1,109 @@
 import { useState } from "react";
-import publishPost from ".././services/Api.js"
-import { useRef,useContext } from "react";
-import UserContext from '../contexts/UserContext';
+//import publishPost from ".././services/Api.js"
+import { publishPost } from "../../services/linkr";
+import { useRef, useContext } from "react";
+import UserContext from "../contexts/UserContext";
 import {
-    Publishstyle,
-    ImgBody,
-    PerfilImg,
-    Form,
-    WhatToday,
-    Inputlink,
-    Inputpost,
-    Button,
-    ButtonArea,
-    ButtonDesative
+	Publishstyle,
+	ImgBody,
+	PerfilImg,
+	Form,
+	WhatToday,
+	Inputlink,
+	Inputpost,
+	Button,
+	ButtonArea,
+	ButtonDesative,
 } from "./styled.js";
 
 export default function Publish() {
-    
-    const linkref = useRef(null);
-    const postref = useRef(null);
+	const linkref = useRef(null);
+	const postref = useRef(null);
 
-    const [link, setLink] = useState('')
-    const [post, setPost] = useState('')
+	const [url, setUrl] = useState("");
+	const [text, setText] = useState("");
 
-    const {pictureUrl,setPictureUrl} = useContext(UserContext)
-    const localPictureUrl = localStorage.getItem('pictureUrl')
-    setPictureUrl(localPictureUrl)
+	const { pictureUrl, setPictureUrl } = useContext(UserContext);
+	const localPictureUrl = localStorage.getItem("pictureUrl");
+	const token = localStorage.getItem("linkrUserToken");
+	setPictureUrl(localPictureUrl);
 
+	//desative buttons and area
+	const [load, setload] = useState(false);
+	const [clicked, setClicked] = useState(false);
 
-    //desative buttons and area
-    const [load, setload] = useState(false)
-    const [clicked, setClicked] = useState(false)
+	function HandleForm(e) {
+		e.preventDefault();
 
-    function HandleForm(e) {
-        e.preventDefault()
+		const infos = {
+			url,
+			text,
+		};
 
-        const infos = {
-            link, post
-        }
+		const promise = publishPost(infos, token);
+		promise.catch((res) => {
+			alert("Houve um erro ao publicar seu link");
+			console.log(res);
+			setClicked(false);
+			setload(false);
+		});
+		promise.then((res) => {
+			alert("Post publicado!");
+			console.log(infos);
+			setClicked(false);
+			linkref.current.value = "";
+			postref.current.value = "";
+		});
+	}
 
-        const promise = publishPost(infos)
-        promise.catch(res => {
-            alert('Houve um erro ao publicar seu link')
-            setClicked(false)
-            setload(false)
-        })
-        promise.then(res => {
-            alert('Post publicado!')
-            console.log(infos)
-            setClicked(false)
-            linkref.current.value = ''
-            postref.current.value = ''
-        })
-    }
+	return (
+		<Publishstyle>
+			<ImgBody>
+				<PerfilImg
+					src={
+						pictureUrl
+							? pictureUrl
+							: "https://yt3.ggpht.com/a/AATXAJyAjXWhg85XlBUBufDpYQ7zB7GIiIlg9js4_wCoFA=s900-c-k-c0xffffffff-no-rj-mo"
+					}
+				/>
+			</ImgBody>
 
-    return (
-        <Publishstyle>
-            <ImgBody>
-                <PerfilImg src={pictureUrl ? pictureUrl : 'https://yt3.ggpht.com/a/AATXAJyAjXWhg85XlBUBufDpYQ7zB7GIiIlg9js4_wCoFA=s900-c-k-c0xffffffff-no-rj-mo'} />
-            </ImgBody>
+			<Form>
+				<WhatToday>What are you going to share today?</WhatToday>
+				<Inputlink
+					type="text"
+					ref={linkref}
+					onChange={(e) => setUrl(e.target.value)}
+					value={url}
+					placeholder="http://..."
+					required
+					disabled={load}
+				/>
+				<Inputpost
+					type="text"
+					ref={postref}
+					onChange={(e) => setText(e.target.value)}
+					value={text}
+					placeholder="Awesome article about #javascript"
+					disabled={load}
+				/>
 
-            <Form>
-                <WhatToday>What are you going to share today?</WhatToday>
-                <Inputlink type='text' ref={linkref} onChange={(e) => setLink(e.target.value)} value={link} placeholder="http://..." required disabled={load} />
-                <Inputpost type='text' ref={postref} onChange={(e) => setPost(e.target.value)} value={post} placeholder="Awesome article about #javascript" disabled={load} />
-
-                <ButtonArea onClick={() => { setClicked(!clicked) }}>
-                    {clicked ?
-                        <ButtonDesative disabled={load}>
-                            <p>Publishing...</p>
-                        </ButtonDesative>
-                        :
-                        <Button onClick={HandleForm} disabled={load}>
-                            <p>Publish</p>
-                        </Button>}
-                </ButtonArea>
-            </Form>
-        </Publishstyle>
-    )
+				<ButtonArea
+					onClick={() => {
+						setClicked(!clicked);
+					}}
+				>
+					{clicked ? (
+						<ButtonDesative disabled={load}>
+							<p>Publishing...</p>
+						</ButtonDesative>
+					) : (
+						<Button onClick={HandleForm} disabled={load}>
+							<p>Publish</p>
+						</Button>
+					)}
+				</ButtonArea>
+			</Form>
+		</Publishstyle>
+	);
 }
